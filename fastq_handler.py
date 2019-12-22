f95d13e25c45e6dc383d4fea634636d23d64f972
chenming <864606804@qq.com>
2019-12-22 22:18:46 +0800

    'add_fasthandler'

--- fastq_handler.py
+++ fastq_handler.py
@@ -14,6 +14,8 @@ class Read():
     aver_qual : Average Base quality
     gc : Average GC 
     N_number : N base number
+
+    Note: This function dosen't consider adaptor up to now
     """
     def __init__(self,fqid,seq,qual,offset=33):
         self.id = fqid
@@ -34,13 +36,32 @@ class Read():
     
     def gc(self):
         gc_num = [base for base in self.seq if base in ['G','g','C','c']]
-        if len(self.seq) == 0:
+        if len(self.seq) == 0: #handling ZeroDivisionError
             return 0
         return gc_num / len(self.seq)
 
-def FastqHandler(fastq_file):
+def FastqHandler(fastq_file,offset=33):
     """
     reading in fastq file and return read object
     """
+
     with open(fastq_file,'r') as indata:
-        for line in indata:
+        while True:
+            line = indata.readline()
+            if line:
+                if not line.startswith('@'):
+                    raise ValueError('Fastq File Format Error the title line does\'t start with @ Please check!')
+                title_line = line.strip()
+                seq_line = indata.readline()
+
+                info_line = indata.readline()
+                if not info_line or info_line[0] != "+":
+                    raise ValueError('Info line should not be empty')
+                
+                bq_line = indata.readline() # only one line is allowed
+                assert bq_line == seq_line ,'Base Quality line length differ from Base sequence line!'
+                
+                yield Read(title_line,seq_line,info_line,offset)
+            else:
+                break
+            
